88280bb56daf22d496ea8e52f9764ad85bbcfd78
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-02 19:39:05 +0900

    [feat] 찍은 이미지를 Glide 로 뷰에 띄우는 기능 구현 (#1)

--- CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/CameraXActivity.kt
+++ CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/CameraXActivity.kt
@@ -16,6 +16,7 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.ikseong.cameraxphotopickerexample.databinding.ActivityCameraxBinding
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -63,9 +64,11 @@ class CameraXActivity : AppCompatActivity() {
 
         // Create output options object which contains file + metadata
         val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(contentResolver,
+            .Builder(
+                contentResolver,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues)
+                contentValues
+            )
             .build()
 
         // Set up image capture listener, which is triggered after photo has
@@ -78,11 +81,15 @@ class CameraXActivity : AppCompatActivity() {
                     Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
 
-                override fun
-                        onImageSaved(output: ImageCapture.OutputFileResults){
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     val msg = "Photo capture succeeded: ${output.savedUri}"
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                     Log.d(TAG, msg)
+
+                    Glide.with(this@CameraXActivity)
+                        .load(output.savedUri)
+                        .into(binding.ivCapturedImage)
+
                 }
             }
         )
@@ -114,9 +121,10 @@ class CameraXActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture)
+                    this, cameraSelector, preview, imageCapture
+                )
 
-            } catch(exc: Exception) {
+            } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
 
