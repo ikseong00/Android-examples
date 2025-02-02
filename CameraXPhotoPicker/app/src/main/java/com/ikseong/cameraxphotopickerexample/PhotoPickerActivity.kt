c71556ac1ee1a7e68743250669e5da2dea46c183
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-02 19:48:46 +0900

    [feat] 갤러리에서 이미지 및 비디오를 가져오는 기능 추가 (#1)

--- CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/PhotoPickerActivity.kt
+++ CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/PhotoPickerActivity.kt
@@ -1,7 +1,11 @@
 package com.ikseong.cameraxphotopickerexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,9 +14,44 @@ import com.ikseong.cameraxphotopickerexample.databinding.ActivityPhotoPickerBind
 class PhotoPickerActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityPhotoPickerBinding.inflate(layoutInflater) }
+    private lateinit var pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setPicker()
+
+        binding.btnSelectSingleImage.setOnClickListener {
+            pickSingleImage()
+        }
+
+        binding.btnSelectVideo.setOnClickListener {
+            pickSingleVideo()
+        }
+
+    }
+
+    private fun setPicker() {
+        pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                // Callback is invoked after the user selects a media item or closes the
+                // photo picker.
+                if (uri != null) {
+                    Log.d("PhotoPicker", "Selected URI: $uri")
+                    binding.ivPickedImage.setImageURI(uri)
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
+    }
+
+    private fun pickSingleImage() {
+        // Launch the photo picker and let the user choose only images.
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun pickSingleVideo() {
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
     }
 }
