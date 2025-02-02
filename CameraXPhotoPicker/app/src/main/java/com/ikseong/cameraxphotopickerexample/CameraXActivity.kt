817011bc350ea6d1eaeaface207db5c79dc5c8d7
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-02 19:29:44 +0900

    [feat] CameraX 액티비티 초기 세팅 (#1)

--- CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/CameraXActivity.kt
+++ CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/CameraXActivity.kt
@@ -1,17 +1,65 @@
 package com.ikseong.cameraxphotopickerexample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.ikseong.cameraxphotopickerexample.databinding.ActivityCameraxBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraXActivity : AppCompatActivity() {
     private val binding by lazy { ActivityCameraxBinding.inflate(layoutInflater) }
 
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // Set up the listeners for take photo and video capture buttons
+        binding.imageCaptureButton.setOnClickListener { takePhoto() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {}
+
+    private fun startCamera() {}
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA,
+            ).apply {
+            }.toTypedArray()
     }
 }
