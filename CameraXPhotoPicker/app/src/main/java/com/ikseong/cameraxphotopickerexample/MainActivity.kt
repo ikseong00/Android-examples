9b411fe607d59858cbf79b3dd2aa864c1b4088b9
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-02 19:24:00 +0900

    [feat] 메인 액티비티 레이아웃 및 intent 로 이동하는 기능 구현

--- CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/MainActivity.kt
+++ CameraXPhotoPicker/app/src/main/java/com/ikseong/cameraxphotopickerexample/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.ikseong.cameraxphotopickerexample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.ikseong.cameraxphotopickerexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(viewBinding.root)
+
+        viewBinding.btnCameraX.setOnClickListener {
+            val intent = Intent(this, CameraXActivity::class.java)
+            startActivity(intent)
         }
+
+        viewBinding.btnPhotoPicker.setOnClickListener {
+            val intent = Intent(this, PhotoPickerActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
