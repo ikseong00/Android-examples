300fee241a61c16cf3e8850923268dd4df28ecb2
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-02 19:18:35 +0900

    [feat] cameraX 의존성 추가 (#1)

--- CameraXPhotoPicker/app/build.gradle.kts
+++ CameraXPhotoPicker/app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // cameraX
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
 }
