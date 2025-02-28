598572ff68b8d176e6c072b8add088ec2aca2bfd
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-28 15:15:15 +0900

    [feat]: 카카오 로그인 함수 구현 (#3)

--- KakaoLogin/app/src/main/java/com/ikseong/kakaologinexample/ui/theme/KakaoLoginScreen.kt
+++ KakaoLogin/app/src/main/java/com/ikseong/kakaologinexample/ui/theme/KakaoLoginScreen.kt
@@ -1,23 +1,33 @@
 package com.ikseong.kakaologinexample.ui.theme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ikseong.kakaologinexample.R
 
 @Composable
-fun KakaoLoginScreen(modifier: Modifier = Modifier) {
+fun KakaoLoginScreen(
+    modifier: Modifier = Modifier,
+    login: () -> Unit = {},
+    getUser: () -> Unit = {},
+    getToken: () -> Unit = {},
+) {
     Box(modifier = modifier.fillMaxSize()) {
         Column(
             modifier = Modifier.align(Alignment.Center),
@@ -34,8 +44,12 @@ fun KakaoLoginScreen(modifier: Modifier = Modifier) {
                 contentDescription = "Kakao Login Button",
                 modifier = Modifier
                     .padding(top = 16.dp)
-                    .padding(horizontal = 20.dp)
+                    .clickable {
+                        login()
+                    }
             )
+
+
         }
     }
 }
