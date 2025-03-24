6081e928a6d0577494f74cda819b250f81d06599
ikseong00 <iksung1289@konkuk.ac.kr>
2025-03-24 14:51:03 +0900

    [feat]: MainActivity 에 NavHost 연결

--- CoilImageCaching/app/src/main/java/com/example/coilimagecaching/MainActivity.kt
+++ CoilImageCaching/app/src/main/java/com/example/coilimagecaching/MainActivity.kt
@@ -4,30 +4,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.compose.AsyncImage
+import coil3.disk.DiskCache
+import coil3.imageLoader
+import coil3.memory.MemoryCache
+import coil3.network.NetworkFetcher
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import coil3.util.DebugLogger
+import com.example.coilimagecaching.navigation.MainNavHost
 import com.example.coilimagecaching.ui.theme.CoilImageCachingTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CoilImageCachingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainNavHost(innerPadding = innerPadding)
                 }
             }
         }
     }
+
 }
 
 @Composable
