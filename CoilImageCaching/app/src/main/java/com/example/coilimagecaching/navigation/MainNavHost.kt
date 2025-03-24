00cdda36e57915308ca16ed9a0ca000af10d8fda
ikseong00 <iksung1289@konkuk.ac.kr>
2025-03-24 14:50:29 +0900

    [feat]: remote 로부터 받아온 imgUrl 들을 렌더링하는 스크린 구현

--- CoilImageCaching/app/src/main/java/com/example/coilimagecaching/navigation/MainNavHost.kt
+++ CoilImageCaching/app/src/main/java/com/example/coilimagecaching/navigation/MainNavHost.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.coilimagecaching.screen.DefaultImageUrlScreen
+import com.example.coilimagecaching.screen.RemoteImageUrlScreen
+import com.example.coilimagecaching.viewmodel.ImageViewModel
 
 
 @Composable
 fun MainNavHost(innerPadding: PaddingValues) {
     val navController = rememberNavController()
+    val imageViewModel: ImageViewModel = viewModel()
 
     NavHost(
         modifier = Modifier.padding(innerPadding),
@@ -26,7 +30,11 @@ fun MainNavHost(innerPadding: PaddingValues) {
             )
         }
         composable<Route.RemoteUrl> {
-
+            RemoteImageUrlScreen(
+                innerPadding = innerPadding,
+                navigateToRemoteUrl = { navController.navigate(Route.DefaultUrl) },
+                viewModel = imageViewModel
+            )
         }
     }
 }
---
+++ CoilImageCaching/app/src/main/java/com/example/coilimagecaching/screen/RemoteImageScreen.kt
@@ -0,0 +1,79 @@
+package com.example.coilimagecaching.screen
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import com.example.coilimagecaching.viewmodel.ImageViewModel
+
+@Composable
+fun RemoteImageUrlScreen(
+    innerPadding: PaddingValues,
+    navigateToRemoteUrl: () -> Unit = {},
+    viewModel : ImageViewModel = viewModel()
+) {
+    val state = rememberLazyListState()
+    val startTime = remember { mutableLongStateOf(System.currentTimeMillis()) }
+    Log.d("RemoteImageUrlScreen", "Start Time: ${startTime.longValue}")
+
+    val imageUrls by viewModel.images.collectAsStateWithLifecycle()
+
+    LazyColumn(
+        state = state,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(imageUrls.size) {
+            AsyncImage(
+                modifier = Modifier.size(100.dp),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrls)
+                    .crossfade(true)
+                    .listener(
+                        onSuccess = { _, result ->
+                            val endTime = System.currentTimeMillis()
+                            val source = result.dataSource
+                            Log.d("RemoteImageUrlScreen", "End Time: $endTime Source: $source")
+                        },
+                    )
+                    .build(),
+                contentDescription = null,
+            )
+        }
+        item {
+            Button(onClick = navigateToRemoteUrl) {
+                Text("Navigate to Remote Url")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RemoteImageUrlScreenPreview() {
+    RemoteImageUrlScreen(
+        innerPadding = PaddingValues(0.dp),
+    )
+}
